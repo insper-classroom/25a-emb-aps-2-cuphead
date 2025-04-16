27ff05852c7a75127b41f3f01810f24f9bda6b7e
Vinicius Grecco <viniciusgfm@al.insper.edu.br>
2025-04-16 18:01:08 -0300

    feat: analogicos

--- main/main.c
+++ main/main.c
@@ -1,137 +1,170 @@
 /*
- * LED blink with FreeRTOS
+ * LED blink with FreeRTOS e filtro média móvel
  */
 #include <FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
 #include <queue.h>
-
-#include "ssd1306.h"
-#include "gfx.h"
-
+#include "hardware/adc.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const uint BTN_1_OLED = 28;
-const uint BTN_2_OLED = 26;
-const uint BTN_3_OLED = 27;
+QueueHandle_t xQueueAdc;
 
-const uint LED_1_OLED = 20;
-const uint LED_2_OLED = 21;
-const uint LED_3_OLED = 22;
+typedef struct adc {
+    int axis;
+    int val;  
+} adc_t;    
 
-void oled1_btn_led_init(void) {
-    gpio_init(LED_1_OLED);
-    gpio_set_dir(LED_1_OLED, GPIO_OUT);
-
-    gpio_init(LED_2_OLED);
-    gpio_set_dir(LED_2_OLED, GPIO_OUT);
-
-    gpio_init(LED_3_OLED);
-    gpio_set_dir(LED_3_OLED, GPIO_OUT);
-
-    gpio_init(BTN_1_OLED);
-    gpio_set_dir(BTN_1_OLED, GPIO_IN);
-    gpio_pull_up(BTN_1_OLED);
-
-    gpio_init(BTN_2_OLED);
-    gpio_set_dir(BTN_2_OLED, GPIO_IN);
-    gpio_pull_up(BTN_2_OLED);
-
-    gpio_init(BTN_3_OLED);
-    gpio_set_dir(BTN_3_OLED, GPIO_IN);
-    gpio_pull_up(BTN_3_OLED);
+void adc_x_task(void *p) {
+    int dataList[5] = {0};
+    int index = 0;
+    int sum = 0;
+    int count = 0;
+    
+    while (1) {
+        adc_init();
+        adc_gpio_init(27);
+        adc_select_input(1);
+        
+        uint16_t result = adc_read();
+        int16_t raw_value = (result - 2047) / 7.96;
+        if (raw_value > -30 && raw_value < 30) {
+            raw_value = 0;
+        }
+        if (count < 5) {
+            dataList[index] = raw_value;
+            sum += raw_value;
+            count++;
+        } else {
+            sum = sum - dataList[index] + raw_value;
+            dataList[index] = raw_value;
+        }
+        index = (index + 1) % 5;
+        int mediamovel = sum / 5;
+        
+        adc_t adc;
+        adc.axis = 0;  
+        adc.val = mediamovel;
+        if (adc.val != 0){
+            xQueueSend(xQueueAdc, &adc, 0);
+        }        
+        vTaskDelay(pdMS_TO_TICKS(50));
+    }
 }
 
-void oled1_demo_1(void *p) {
-    printf("Inicializando Driver\n");
-    ssd1306_init();
-
-    printf("Inicializando GLX\n");
-    ssd1306_t disp;
-    gfx_init(&disp, 128, 32);
-
-    printf("Inicializando btn and LEDs\n");
-    oled1_btn_led_init();
-
-    char cnt = 15;
+void adc_y_task(void *p) {
+    int dataList[5] = {0};
+    int index = 0;
+    int sum = 0;
+    int count = 0;
+    
     while (1) {
-
-        if (gpio_get(BTN_1_OLED) == 0) {
-            cnt = 15;
-            gpio_put(LED_1_OLED, 0);
-            gfx_clear_buffer(&disp);
-            gfx_draw_string(&disp, 0, 0, 1, "LED 1 - ON");
-            gfx_show(&disp);
-        } else if (gpio_get(BTN_2_OLED) == 0) {
-            cnt = 15;
-            gpio_put(LED_2_OLED, 0);
-            gfx_clear_buffer(&disp);
-            gfx_draw_string(&disp, 0, 0, 1, "LED 2 - ON");
-            gfx_show(&disp);
-        } else if (gpio_get(BTN_3_OLED) == 0) {
-            cnt = 15;
-            gpio_put(LED_3_OLED, 0);
-            gfx_clear_buffer(&disp);
-            gfx_draw_string(&disp, 0, 0, 1, "LED 3 - ON");
-            gfx_show(&disp);
+        adc_init();
+        adc_gpio_init(26);
+        adc_select_input(0);
+        
+        uint16_t result = adc_read();
+        int16_t raw_value = (result - 2047) / 7.96;
+        if (raw_value > -30 && raw_value < 30) {
+            raw_value = 0;
+        }
+        
+        if (count < 5) {
+            dataList[index] = raw_value;
+            sum += raw_value;
+            count++;
         } else {
+            sum = sum - dataList[index] + raw_value;
+            dataList[index] = raw_value;
+        }
+        index = (index + 1) % 5;
+        int mediamovel = sum / 5;
+        
+        adc_t adc;
+        adc.axis = 1;  
+        adc.val = mediamovel;
+        if (adc.val != 0){
+            xQueueSend(xQueueAdc, &adc, 0);
 
-            gpio_put(LED_1_OLED, 1);
-            gpio_put(LED_2_OLED, 1);
-            gpio_put(LED_3_OLED, 1);
-            gfx_clear_buffer(&disp);
-            gfx_draw_string(&disp, 0, 0, 1, "PRESSIONE ALGUM");
-            gfx_draw_string(&disp, 0, 10, 1, "BOTAO");
-            gfx_draw_line(&disp, 15, 27, cnt,
-                          27);
-            vTaskDelay(pdMS_TO_TICKS(50));
-            if (++cnt == 112)
-                cnt = 15;
+        }        
+        vTaskDelay(pdMS_TO_TICKS(50));
+    }
+}
 
-            gfx_show(&disp);
+void adc2_y_task(void *p){
+    int dataList[5] = {0};
+    int index = 0;
+    int sum = 0;
+    int count = 0;
+    while(1){
+        adc_init();
+        adc_gpio_init(); //dps colocar o numero q agt escolher aqui
+        adc_select_input(); //input vai depender daquela tabela
+
+        uint16_t result = adc_read();
+        int16_t raw_value = (result-2047)/7.96;
+        if (raw_value>-200 && raw_value < 200){
+            raw_value = 0;
+        }
+
+        if (count < 5) {
+            dataList[index] = raw_value;
+            sum += raw_value;
+            count++;
+        } else {
+            sum = sum - dataList[index] + raw_value;
+            dataList[index] = raw_value;
+        }
+        index = (index + 1) % 5;
+        int mediamovel = sum / 5;
+        
+        adc_t adc;
+        adc.axis = 3;  
+        adc.val = mediamovel;
+        if (adc.val != 0){
+            xQueueSend(xQueueAdc, &adc, 0);
+
+        }        
+        vTaskDelay(pdMS_TO_TICKS(50));
+
+
+    }
+}
+
+void uart_task(void *p) {
+    adc_t adc;
+    while (1) {
+        if (xQueueReceive(xQueueAdc, &adc, portMAX_DELAY)) {
+            int16_t val = adc.val;
+            uint8_t axis = adc.axis;
+            uint8_t val_0 = val & 0xFF;
+            uint8_t val_1 = (val >> 8) & 0xFF;
+            uint8_t sync = 0xFF; 
+            putchar(sync);
+            putchar(axis);
+            putchar(val_0);
+            putchar(val_1);
+        
+
+            
         }
     }
 }
 
-void oled1_demo_2(void *p) {
-    printf("Inicializando Driver\n");
-    ssd1306_init();
-
-    printf("Inicializando GLX\n");
-    ssd1306_t disp;
-    gfx_init(&disp, 128, 32);
-
-    printf("Inicializando btn and LEDs\n");
-    oled1_btn_led_init();
-
-    char cnt = 15;
-    while (1) {
-
-        gfx_clear_buffer(&disp);
-        gfx_draw_string(&disp, 0, 0, 1, "Mandioca");
-        gfx_show(&disp);
-        vTaskDelay(pdMS_TO_TICKS(150));
-
-        gfx_clear_buffer(&disp);
-        gfx_draw_string(&disp, 0, 0, 2, "Batata");
-        gfx_show(&disp);
-        vTaskDelay(pdMS_TO_TICKS(150));
-
-        gfx_clear_buffer(&disp);
-        gfx_draw_string(&disp, 0, 0, 4, "Inhame");
-        gfx_show(&disp);
-        vTaskDelay(pdMS_TO_TICKS(150));
-    }
-}
 
 int main() {
     stdio_init_all();
-
-    xTaskCreate(oled1_demo_2, "Demo 2", 4095, NULL, 1, NULL);
-
+    
+    xQueueAdc = xQueueCreate(32, sizeof(adc_t));
+    
+    xTaskCreate(adc_x_task, "adc x task", 4095, NULL, 1, NULL);
+    xTaskCreate(adc_y_task, "adc y task", 4095, NULL, 1, NULL);
+    xTaskCreate(uart_task, "uart task", 4095, NULL, 1, NULL);
+    xTaskCreate(adc2_y_task, "adc2 y task", 4095, NULL, 1, NULL);
+    
     vTaskStartScheduler();
-
+    
     while (true)
         ;
 }

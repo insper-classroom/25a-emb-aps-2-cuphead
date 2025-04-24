3479b7b77a728857d8eadbf3fe2ff26fde09ed58
Vinicius Grecco <viniciusgfm@al.insper.edu.br>
2025-04-24 00:14:07 -0300

    feat: callbacks

--- main/main.c
+++ main/main.c
@@ -1,11 +1,12 @@
 /*
- * LED blink with FreeRTOS e filtro média móvel
+ * LED blink with FreeRTOS e filtro média móvel + interrupção BTN_LIGA
  */
 #include <FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
 #include <queue.h>
 #include "hardware/adc.h"
+#include "hardware/gpio.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
 
@@ -135,8 +136,8 @@ void desh_y_task(void *p){
         vTaskDelay(pdMS_TO_TICKS(50));
 
 
-     }
- }
+    }
+}
 
 void uart_task(void *p) {
     adc_t adc;
@@ -151,40 +152,110 @@ void uart_task(void *p) {
             putchar(axis);
             putchar(val_0);
             putchar(val_1);
-        
 
-            
+
+
         }
     }
 }
 
 
+void btn_liga_callback(uint gpio, uint32_t events) {
+    if (events == 0x4) { // borda de descida
+    } else if (events == 0x8) { // borda de subida
+    }
+}
+
+void btn_troca_callback(uint gpio, uint32_t events) {
+    if (events == 0x4) { // borda de descida
+    } else if (events == 0x8) { // borda de subida
+    }
+}
+
+void btn_atira_callback(uint gpio, uint32_t events) {
+    if (events == 0x4) { // borda de descida
+    } else if (events == 0x8) { // borda de subida
+    }
+}
+
+void btn_pika_callback(uint gpio, uint32_t events) {
+    if (events == 0x4) { // borda de descida
+    } else if (events == 0x8) { // borda de subida
+    }
+}
+
+void btn_pausa_callback(uint gpio, uint32_t events) {
+    if (events == 0x4) { // borda de descida
+    } else if (events == 0x8) { // borda de subida
+    }
+}
+
 int main() {
     stdio_init_all();
+
+    // Configuração do botão BTN_LIGA com interrupção
     gpio_init(BTN_LIGA);
-    gpio_set_dir(BTN_LIGA, GPIO_OUT);
+    gpio_set_dir(BTN_LIGA, GPIO_IN);
+    gpio_pull_up(BTN_LIGA);
 
     gpio_init(BTN_TROCA);
-    gpio_set_dir(BTN_TROCA, GPIO_OUT);
+    gpio_set_dir(BTN_TROCA, GPIO_IN);
+    gpio_pull_up(BTN_TROCA);
 
     gpio_init(BTN_ATIRA);
-    gpio_set_dir(BTN_ATIRA, GPIO_OUT);
-    
+    gpio_set_dir(BTN_ATIRA, GPIO_IN);
+    gpio_pull_up(BTN_ATIRA);
+
     gpio_init(BTN_PIKA);
-    gpio_set_dir(BTN_PIKA, GPIO_OUT);
+    gpio_set_dir(BTN_PIKA, GPIO_IN);
+    gpio_pull_up(BTN_PIKA);
 
     gpio_init(BTN_PAUSA);
-    gpio_set_dir(BTN_PAUSA, GPIO_OUT);
+    gpio_set_dir(BTN_PAUSA, GPIO_IN);
+    gpio_pull_up(BTN_PAUSA);
+
+    gpio_set_irq_enabled_with_callback(
+        BTN_LIGA,
+        GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+        true,
+        &btn_liga_callback
+    );
+
+    gpio_set_irq_enabled_with_callback(
+        BTN_TROCA,
+        GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+        true,
+        &btn_troca_callback
+    );
+    gpio_set_irq_enabled_with_callback(
+        BTN_ATIRA,
+        GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+        true,
+        &btn_atira_callback
+    );
+    gpio_set_irq_enabled_with_callback(
+        BTN_PIKA,
+        GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+        true,
+        &btn_pika_callback
+    );
+    gpio_set_irq_enabled_with_callback(
+        BTN_PAUSA,
+        GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
+        true,
+        &btn_pausa_callback
+    );
+
 
     xQueueAdc = xQueueCreate(32, sizeof(adc_t));
-    
+
     xTaskCreate(adc_x_task, "adc x task", 4095, NULL, 1, NULL);
     xTaskCreate(adc_y_task, "adc y task", 4095, NULL, 1, NULL);
     xTaskCreate(uart_task, "uart task", 4095, NULL, 1, NULL);
     xTaskCreate(desh_y_task, "adc2 y task", 4095, NULL, 1, NULL);
-    
+
     vTaskStartScheduler();
-    
-    while (true)
-        ;
+
+    while (true) 
+    ;
 }
